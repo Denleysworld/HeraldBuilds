99e86c0ea28c3d88429014a9a1fe26f8ec835695
Celestial Renegade <llime6260@gmail.com>
2025-07-01 18:34:55 +0300

    Homepage in React

---
+++ homepage_re/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="site-title">
+        <img
+          src="/Images/Logo/Heraldbuilds logo.png"
+          alt="Herald Builds Logo"
+          className="site-logo"
+        />
+        <span>Herald Builds</span>
+      </div>
+      <nav className="nav-links">
+        <a href="../Home/index.html">Home</a>
+        <a href="../Troubleshooting/troubleshooting.html">Troubleshooting</a>
+        <a href="../Guides/guides.html">PC Guides</a>
+        <a href="../Tools/tools.html">Tools</a>
+        <a href="../About/about.html">About Us</a>
+        <a href="../FAQ/faq.html">FAQ</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ homepage_re/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public/Images',
+          to: 'Images',
+        },
+      ],
+    }),
+  ],
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname, 'dist'),
+      },
+      {
+        directory: path.join(__dirname, 'public'),
+        publicPath: '/',
+      }
+    ],
+    compress: true,
+    port: 3000,
+    open: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
