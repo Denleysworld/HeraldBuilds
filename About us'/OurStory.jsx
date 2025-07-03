3de185f7bf107d33909ea4b96aeb2324ec46f235
Ian <146425348+Karma19567@users.noreply.github.com>
2025-07-03 20:38:44 +0300

    Add files via upload

---
+++ About us'/OurStory.jsx
@@ -0,0 +1,23 @@
+const OurStory = () => {
+  return (
+    <section className="our-story">
+      <h2>Our Story</h2>
+      <div className="story-content">
+        <p>
+          Herald Builds was founded by a group of 4 passionate PC enthusiasts
+          who believed that building a computer should be accessible to
+          everyone. What started as a small community of tech enthusiasts
+          helping friends build their first PCs has grown into a comprehensive
+          resource for PC builders worldwide
+        </p>
+        <p>
+          Based in Nairobi, Kenya, we're proud to be contributing to the
+          growing tech community in East Africa while serving PC builders
+          globally
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default OurStory;
