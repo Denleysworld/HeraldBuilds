beaef528ad0d3838b3886591c1e841831b2a2dfc
Ian <146425348+Karma19567@users.noreply.github.com>
2025-07-03 21:10:26 +0300

    Footer.jsx

--- heraldbuilds/src/About us/Footer.jsx
+++ heraldbuilds/src/About us/Footer.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../styles/About_styles.css';
 import { 
   faFacebook, 
   faTwitter, 
