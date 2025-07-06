8c87c3e1ae9106350de4a3c56453d056fd1b4714
denley <deekayel05@gmail.com>
2025-07-06 22:43:31 +0300

    image

--- heraldbuilds/src/About us/OurTeam.jsx
+++ heraldbuilds/src/About us/OurTeam.jsx
@@ -1,25 +1,30 @@
+import avatar1 from '../components/pages/images/avatars/avatar1.jpg';
+import avatar2 from '../components/pages/images/avatars/avatar2.jpg';
+import avatar3 from '../components/pages/images/avatars/avatar3.jpg';
+import avatar4 from '../components/pages/images/avatars/avatar4.jpg';
+
 const OurTeam = () => {
   const teamMembers = [
     {
-      imgSrc: "../Images/avatars/avatar1.jpg",
+      imgSrc: avatar1,
       name: "Sheel Chudasama",
       role: "Co-Founder & Technical Lead",
       bio: "Expert in PC hardware optimization and system architecture design"
     },
     {
-      imgSrc: "../Images/avatars/avatar2.jpg",
+      imgSrc: avatar2,
       name: "Denley Kipkoech",
       role: "Co-Founder & Content Strategist",
       bio: "Specializes in creating comprehensive PC building guides and tutorials"
     },
     {
-      imgSrc: "../Images/avatars/avatar3.jpg",
+      imgSrc: avatar3,
       name: "Griffin Sitati",
       role: "Co-Founder & UX Designer",
       bio: "Focuses on making PC building knowledge accessible through intuitive design"
     },
     {
-      imgSrc: "../Images/avatars/avatar4.jpg",
+      imgSrc: avatar4,
       name: "Ian Kabachia",
       role: "Co-Founder & Development Lead",
       bio: "Drives the technical development of our platform and troubleshooting tools"
